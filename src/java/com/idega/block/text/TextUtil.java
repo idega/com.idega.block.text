e3302c2e96a5939ce2cf1342ce0266e880c5cac1
Valdas Žemaitis <valdas@idega.com>
2022-08-05 11:51:51 +0300

    New methods

--- src/java/com/idega/block/text/TextUtil.java
+++ src/java/com/idega/block/text/TextUtil.java
@@ -2,21 +2,28 @@ package com.idega.block.text;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
 
+import com.idega.block.text.data.bean.LocalizedText;
 import com.idega.block.text.model.LocalizedTextModel;
 import com.idega.core.localisation.business.ICLocaleBusiness;
 import com.idega.util.ListUtil;
+import com.idega.util.StringUtil;
 
 public class TextUtil {
 
 	private TextUtil() {}
 
-	public static final <T extends LocalizedTextModel> T getLocalizedText(Collection<T> texts, Locale locale) {
+	public static final int getICLocaleId(Locale locale) {
 		if (locale == null) {
 			locale = ICLocaleBusiness.getLocaleFromLocaleString(Locale.ENGLISH.toString());
 		}
-		int localeId = ICLocaleBusiness.getLocaleId(locale);
+		return ICLocaleBusiness.getLocaleId(locale);
+	}
+
+	public static final <T extends LocalizedTextModel> T getLocalizedText(Collection<T> texts, Locale locale) {
+		int localeId = getICLocaleId(locale);
 		return getLocalizedText(texts, localeId);
 	}
 
@@ -33,6 +40,21 @@ public class TextUtil {
 		}
 
 		return null;
+	}
+
+	public static final String getLocalized(List<LocalizedText> texts, int icLocaleId) {
+		LocalizedText text = getLocalizedText(texts, icLocaleId);
+		if (text == null) {
+			return null;
+		}
+
+		String localized = text.getBody();
+		localized = StringUtil.isEmpty(localized) ? text.getHeadline() : localized;
+		return StringUtil.isEmpty(localized) ? text.getTitle() : localized;
+	}
+
+	public static final String getLocalized(List<LocalizedText> texts, Locale locale) {
+		return getLocalized(texts, getICLocaleId(locale));
 	}
 
 }
